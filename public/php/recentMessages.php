534fe65cb2337ce9949faf519ee67c535aa07243
Pascal Niklas Paul <admin@example.com>
2019-06-22 09:44:05 +0000

    Add comment with new endpoint URLs in recentMessages.php

--- public/php/recentMessages.php
+++ public/php/recentMessages.php
@@ -1,4 +1,6 @@
 <?php
+  // https://recent-messages.robotty.de/api/v2/recent-messages/c0ldplasma
+  // https://thomassen.sh/twitch-api-endpoints/
   $apiURL = "https://tmi.twitch.tv/api/rooms/" . $_GET['chatId'] . "/recent_messages?count=50";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
